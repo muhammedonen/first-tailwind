cb00deef95fc3e5384a34f5adc7cc6e81224b1af
muhammedonen <mufaah@windowslive.com>
2023-02-04 22:51:24 +0300

    header_add_menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        quicsand: ["Quicksand", "sans-serif"],
+        sansPro: ["Source Sans Pro", "sans-serif"],
+      },
+      colors: {
+        mainTheme: "#E3F6FF",
+        mainBtn1: "#1E3163",
+        mainBtn2: "#1B262C",
+        mainBorder: "#F4F4F4",
+        mainTheme: "#E3F6FF",
+        btnText: "#DADADA",
+        mainLight: "#F6F6F6",
+        hoverText: "#EB596E",
+      },
+    },
   },
   plugins: [],
 };
